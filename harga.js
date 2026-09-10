2b5f8dcd853ecdff162358f32a078813555e85a6
purbalinggabmtc-cmyk <purbalinggabmtc@gmail.com>
2026-09-10 23:55:30 +0700

    Create harga.js

---
+++ harga.js
@@ -0,0 +1,28 @@
+const hargaProduk = {
+
+    prostBeer:{
+        nama:"Prost Beer",
+        harga:55000
+    },
+
+    laBold16:{
+        nama:"LA Bold 16",
+        harga:50000
+    },
+
+    kaos:{
+        nama:"Kaos Event",
+
+        ukuran:{
+            M:100000,
+            L:100000,
+            XL:100000,
+            "2XL":105000,
+            "3XL":110000,
+            "4XL":115000
+        },
+
+        lenganPanjang:10000
+    }
+
+};
