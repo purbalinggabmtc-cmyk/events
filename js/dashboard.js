b0f3f924348d90156da475414c7e5ce8d8008d50
purbalinggabmtc-cmyk <purbalinggabmtc@gmail.com>
2026-09-11 20:42:38 +0700

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,64 @@
+const peserta =
+
+JSON.parse(
+
+localStorage.getItem("peserta")
+
+);
+
+
+
+if(!peserta){
+
+
+window.location.href="index.html";
+
+
+}
+
+
+
+
+
+document.getElementById("nama")
+
+.innerHTML = peserta.nama;
+
+
+
+
+document.getElementById("id")
+
+.innerHTML = peserta.id;
+
+
+
+
+document.getElementById("tipe")
+
+.innerHTML = peserta.tipe;
+
+
+
+
+
+
+new QRCode(
+
+document.getElementById("qrcode"),
+
+{
+
+
+text:peserta.barcode,
+
+
+width:160,
+
+
+height:160
+
+
+}
+
+);
